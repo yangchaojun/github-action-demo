9ce9fb2a4c03b85c274a5abd7ba0969a795b08f2
yangchaojun <yangchaojunxs@126.com>
2020-05-18 15:03:06 +0800

    添加数据请求

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import axios from 'axios'
 import logo from './logo.svg'
 import './App.css'
 
 function App() {
+  async function getData() {
+    let result = await axios.get('http://jqapi.hao1bao.com/app', {
+      params: {
+        op: 'Page',
+        cloud: 'index_roles',
+        page: 1,
+        rows: 10,
+        _userid: 1,
+        _password: '25d55ad283aa400af464c76d713c07ad'
+      }
+    })
+
+    console.log(result)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p onClick={getData}>
           Edit2222 <code>src/App.js</code> and save to reload.
         </p>
         <a
